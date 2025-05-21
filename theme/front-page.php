3e989a929b33299a9f39ca4f6f422a62b0023986
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-05-21 13:32:50 -0400

    add button util

--- theme/front-page.php
+++ theme/front-page.php
@@ -20,4 +20,9 @@ get_header();
     <p>Hey, take a look at the earthlings. Goodbye! Life finds a way. I was part of something special. Life finds a way. What do they got in there? King Kong? God creates dinosaurs. God destroys dinosaurs. God creates Man. Man destroys God. Man creates Dinosaurs.</p>
 </section>
 
+<section class="wrapper py-xl">
+    <a href="https://www.lebronconsulting.tech" class="btn outline-2 outline-primary">Lebron Consulting</a>
+    <a href="https://www.lebronconsulting.tech" class="btn bg-foreground text-background">Lebron Consulting</a>
+</section>
+
 <?php get_footer(); ?>
