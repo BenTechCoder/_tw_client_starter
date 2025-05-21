dd607114aa7ef986361fd66935742a851ce20ba6
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-05-21 12:31:02 -0400

    add utopia spacing and tpography

---
+++ theme/front-page.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * The Homepage
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package _tw
+ */
+
+get_header();
+
+
+?>
+
+<section class="py-xl text-step">
+    <h1>HERO</h1>
+    <h2>HEADING</h2>
+    <h3>SUBHEADING</h3>
+    <p>Hey, take a look at the earthlings. Goodbye! Life finds a way. I was part of something special. Life finds a way. What do they got in there? King Kong? God creates dinosaurs. God destroys dinosaurs. God creates Man. Man destroys God. Man creates Dinosaurs.</p>
+</section>
+
+<?php get_footer(); ?>
